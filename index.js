cdde10255cd56ccc8643c585eb69475d4ba6a93f
Mehedi62544Hasane <mehedi62544hasan@gmail.comm>
2022-10-25 07:13:29 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+const Port = process.env.Port || 5000;
+
+app.get("/", (req, res) =>{
+    res.send("new server is running")
+})
+
+
+app.listen(Port, () => {
+    console.log("server is running", Port);
+});
